06d790a40457e33e48b1c3a2081d9637aeaeb617
kasturi4237 <157556416+kasturi4237@users.noreply.github.com>
2024-11-02 20:41:25 +0000

    conversion

---
+++ 01_basics/03_conversion_operation.js
@@ -0,0 +1,13 @@
+let score1="33abd"
+let score2=null
+let score3=false
+console.log(typeof score1);
+console.log(typeof(score2));
+console.log(typeof score3);
+let valueInNumber= Number(score3)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn=0
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
